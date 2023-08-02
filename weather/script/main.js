b4ebf5e2fa30004a12608b722bcfd06a3a69e1b8
YoungAe Sa <duddo0305@gmail.com>
2023-08-02 09:37:29 +0900

    weather

--- weather/script/main.js
+++ weather/script/main.js
@@ -1,59 +1,85 @@
-window.onload= function(){
-    const input = document.querySelector('#search');
-    console.log(input);
 
-    input.onfocus = function () {
-        browsers.style.display = 'block';
-        input.style.borderRadius = "5px 5px 0 0";  
-      };
-      for (let option of browsers.options) {
-        option.onclick = function () {
-          input.value = option.value;
-          browsers.style.display = 'none';
-          input.style.borderRadius = "5px";
+window.onload = function() {
+  const API_KEY = '1dc69a07c545e83623a0d4ca51096867';
+  
+  // 날씨 정보 가져오기
+  function fetchWeather(cityName) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cityName)}&appid=${API_KEY}&cnt=5&units=metric`;
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const city = document.querySelector('.world_loc');
+        const temper = document.querySelectorAll('.temper');
+        const weather = document.querySelectorAll('.weather_icon > img');
+        const apparent = document.querySelector('.apparent > p > .data');
+        const humi = document.querySelector('.humidity > p > .data');
+        const wind = document.querySelector('.wind > p > .data');
+        const weatherId = data.weather[0].id;
+        const weatherGradi = document.querySelector('#weather-gradient');
+        
+        city.innerText = data.name;
+        temper[0].innerText = Math.round(data.main.temp);
+        temper[1].innerText = Math.round(data.main.temp);
+
+        weather[0].src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+        weather[1].src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+        apparent.innerHTML = Math.round(data.main.feels_like);
+        humi.innerHTML = data.main.humidity;
+        wind.innerHTML = data.wind.speed;
+        
+        // 날씨에 따른 배경색 변경
+        weatherGradi.classList.remove('cloudy', 'clean', 'rainy', 'sunny');
+        if (weatherId === 800) {
+          weatherGradi.classList.add('clean');
+        } else if (200 <= weatherId && weatherId < 400 || 600 <= weatherId && weatherId < 799 || 801 <= weatherId && weatherId < 900) {
+          weatherGradi.classList.add('cloudy');
+        } else if (500 <= weatherId && weatherId < 600) {
+          weatherGradi.classList.add('rainy');
+        } else {
+          weatherGradi.classList.add('sunny');
         }
-      };
-      
-      input.oninput = function() {
-        currentFocus = -1;
-        var text = input.value.toUpperCase();
-        for (let option of browsers.options) {
-          if(option.value.toUpperCase().indexOf(text) > -1){
-            option.style.display = "block";
-        }else{
-          option.style.display = "none";
-          }
-        };
-      }
-      var currentFocus = -1;
-      input.onkeydown = function(e) {
-        if(e.keyCode == 40){
-          currentFocus++
-         addActive(browsers.options);
-        }
-        else if(e.keyCode == 38){
-          currentFocus--
-         addActive(browsers.options);
-        }
-        else if(e.keyCode == 13){
-          e.preventDefault();
-              if (currentFocus > -1) {
-                /*and simulate a click on the "active" item:*/
-                if (browsers.options) browsers.options[currentFocus].click();
-              }
-        }
-      }
-      
-      function addActive(x) {
-          if (!x) return false;
-          removeActive(x);
-          if (currentFocus >= x.length) currentFocus = 0;
-          if (currentFocus < 0) currentFocus = (x.length - 1);
-          x[currentFocus].classList.add("active");
-        }
-        function removeActive(x) {
-          for (var i = 0; i < x.length; i++) {
-            x[i].classList.remove("active");
-          }
-        }
+        
+        console.log('데이터 확인!', data);
+      });
+  }
+
+  // 도시 선택 옵션 변경 시 이벤트 처리
+  let select = document.getElementById('cityName');
+  select.addEventListener("change", function(e) {
+    const cityName = e.target.value;
+    fetchWeather(cityName);
+  });
+  
+  // 현재 위치의 날씨 정보 가져오기
+  function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    fetchWeather("your current location");
+  }
+
+  function handleGeoError() {
+    console.log('현재 위치를 가져올 수 없습니다.');
+  }
+  
+  // 현재 위치 정보 가져오기
+  navigator.geolocation.getCurrentPosition(onGeoOk, handleGeoError);
+
+  // 오늘의 날짜 정보 표시
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let day = today.getDate();
+  const WEEKDAY = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  let week = WEEKDAY[today.getDay()].slice(0, 3);
+  
+  document.querySelector('.days').innerText = week;
+  document.querySelector('.month').innerText = month;
+  document.querySelector('.year').innerText = year;
+
+  let select_day = Array.from(document.querySelectorAll('.select_day .day'));
+  
+  for (i = 0; i < 6; i++) {
+    select_day[i].innerHTML =  WEEKDAY[today.getDay()+i].slice(0, 3);
+  }
 }
