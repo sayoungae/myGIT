939a9d8e78f080ee8a3aa952574ada2b86c4c900
YoungAe Sa <duddo0305@gmail.com>
2023-08-02 10:34:45 +0900

    weather

--- weather/script/main.js
+++ weather/script/main.js
@@ -4,7 +4,7 @@ window.onload = function() {
   
   // 날씨 정보 가져오기
   function fetchWeather(cityName) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cityName)}&appid=${API_KEY}&cnt=5&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&cnt=5&units=metric`;
     
     fetch(url)
       .then(response => response.json())
@@ -18,9 +18,9 @@ window.onload = function() {
         const weatherGradi = document.querySelector('#weather-gradient');
 
         let weatherId = data.weather[0].id;
-        city.innerText = data.name;
-        temper[0].innerText = Math.round(data.main.temp);
-        temper[1].innerText = Math.round(data.main.temp);
+        city.textContent = data.name;
+        temper[0].textContent = Math.round(data.main.temp);
+        temper[1].textContent = Math.round(data.main.temp);
 
         weather[0].src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
         weather[1].src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
@@ -47,23 +47,23 @@ window.onload = function() {
   // 도시 선택 옵션 변경 시 이벤트 처리
   let select = document.getElementById('cityName');
   select.addEventListener("change", function(e) {
-    const cityName = e.target.value;
+    cityName = e.target.value;
     fetchWeather(cityName);
   });
   
   // 현재 위치의 날씨 정보 가져오기
-  function onGeoOk(position) {
+/*   function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     fetchWeather("your current location");
-  }
+  } */
 
-  function handleGeoError() {
+  /* function handleGeoError() {
     console.log('현재 위치를 가져올 수 없습니다.');
-  }
+  } */
   
   // 현재 위치 정보 가져오기
-  navigator.geolocation.getCurrentPosition(onGeoOk, handleGeoError);
+  // navigator.geolocation.getCurrentPosition(onGeoOk, handleGeoError);
 
   // 오늘의 날짜 정보 표시
   let today = new Date();
@@ -73,15 +73,15 @@ window.onload = function() {
   const WEEKDAY = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   let week = WEEKDAY[today.getDay()].slice(0, 3);
   console.log(WEEKDAY)
-  document.querySelector('.days').innerText = week;
-  document.querySelector('.month').innerText = month;
-  document.querySelector('.year').innerText = year;
+  document.querySelector('.days').textContent = week;
+  document.querySelector('.month').textContent = month;
+  document.querySelector('.year').textContent = year;
 
   let select_day = document.querySelectorAll('.select_day .day');
   let select_arr = new Array();
   for (i = 0; i < 4; i++) {
     select_arr.push(WEEKDAY[(today.getDay() + i) % 7]);
     // console.log(select_arr, typeof(select_arr))
-    select_day[i].innerText = select_arr[i].slice(0,3)
+    select_day[i].textContent = select_arr[i].slice(0,3)
   }
 }
